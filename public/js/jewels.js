0d7fcf9056d7e69a1483e4de8ff8ad45a7feb710
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-11-13 19:38:25 -0400

    Base del proyecto

---
+++ public/js/jewels.js
@@ -0,0 +1,91 @@
+export default [
+    {
+        "id": "JANI0001",
+        "title": "Anillo Oro Amarillo y Blanco",
+        "image_path": "jewels/JANI0001.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+
+    {
+        "id": "JANI0002",
+        "title": "Anillo Oro Blanco Diamantes",
+        "image_path": "jewels/JANI0002.jpg",
+        "category" : "jewels"
+    },
+
+    {
+        "id": "JANI0003",
+        "title": "Anillo Oro Blanco Diamantes",
+        "image_path": "jewels/JANI0003.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JANI0004",
+        "title": "Anillo Oro Blanco Diamantes",
+        "image_path": "jewels/JANI0004.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JANI0005",
+        "title": "Anillo Oro Blanco Diamantes",
+        "image_path": "jewels/JANI0005.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JANI0006",
+        "title": "Anillo Oro Amarillo Cartier",
+        "image_path": "jewels/JANI0006.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JCAD0001",
+        "title": "Cadena Edtion Special Titanic",
+        "image_path": "jewels/JCAD0001.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JCON0001",
+        "title": "Anillo Oro Amarillo y Blanco",
+        "image_path": "jewels/JCON0001.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JPUL0001",
+        "title": "Pulsera Oro Blanco con Diamantes",
+        "image_path": "jewels/JPUL0001.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+    {
+        "id": "JZAR0001",
+        "title": "Zarcillo oro Amarillo y Blanco",
+        "image_path": "jewels/JZAR0001.jpg",
+        "category": "jewels",
+        "price": 1500
+    },
+
+
+    {
+        "id": "RBVL0001",
+        "title": "BVULGARI Watch",
+        "image_path": "clocks/RBVL0001.jpg",
+        "category": "clocks",
+        "price": 15000
+    },
+
+    {
+        "id": "RCOR0001",
+        "title": "CORUM Diamont",
+        "image_path": "clocks/RCOR0001.jpg",
+        "category": "clocks",
+        "price": 15000
+    },
+
+]
