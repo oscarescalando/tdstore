0aba160be543d3e62de1a27329e492fb63d32c6c
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-11-18 23:43:13 -0400

    nuevas funcionalidades

--- public/js/appjewel.js
+++ public/js/appjewel.js
@@ -1,4 +1,21 @@
-import domicile from './domicile.js';
 import render from './render.js';
 
-render(domicile);
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('./js/domicile.json')
+        const data = await res.json()
+
+        render(data);
+        //console.log(data)
+        //pintarProductos(data)
+        //detectarBotones(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
--- public/js/render.js
+++ public/js/render.js
@@ -9,31 +9,45 @@ function renderElement(store) {
   window.container.append(element)
 }
 
-function buildElement({ title, description, image_path, image, category, id, price, info, order }) {
+function buildElement({ title, description, image_path, image, category, id, price, info, order, extra }) {
 
-    let image_div = '';
+  let image_div = '';
+  let link = '';
+
+  if (extra) {
+    link = 'id="'+ category + '"';
+  }
 
     if (image) {
         image_div = `
             <div class="bg-white md:rounded-r-none rounded-lg">
-                <img loading="lazy"class=" object-cover object-left w-full h-full rounded-lg md:w-56 md:h-full md:rounded-r-none" src="images/${image_path}" alt="${title}">
+                <img loading="lazy"class=" object-cover object-left w-full
+                h-full rounded-lg md:w-56 md:h-full md:rounded-r-none"
+                src="images/${image_path}" alt="${title}">
             </div>
         `;
 
     }
+  
+  let _info = "";
+  
+  if (info.length > 0) {
+    //console.log(info);
+    info.forEach(element =>  _info +=` <p class="mt-2 leading-snug text-gray-700 ">${element}</p>`);
+  }
 
     const template = `
     
-     <div class="mx-2 mt-4 rounded-lg md:mx-0 md:w-1/2 md:flex md:bg-white ">
+     <div class="mx-2 mt-4 rounded-lg md:mx-0 md:w-1/2 md:flex md:bg-white " ${link} data-category="${category}" data-id="${id}">
         
                  ${image_div}
 
-                <div class="relative w-full p-2 mx-2 -mt-2 bg-white rounded-lg shadow-lg md:bg-transparent">
+                <div class="relative  p-2 mx-2 -mt-2 bg-white rounded-lg shadow-lg md:bg-transparent">
                     <h2 class="text-xl font-semibold tracking-tight text-base uppercase">${title} </h2>
                     <p class="mt-2 leading-snug text-gray-700 ">
                     ${description}
                     </p>
-                   
+                    ${_info}
                     <div class="mt-2 text-lg font-semibold text-right text-base">${price}</div>
                    
                 </div>
